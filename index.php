edbba63e25321cc2e6558ff2cfc97d03279f8705
JW Junior <jwjuniorsi@gmail.com>
2018-08-26 18:48:27 -0300

    Inserir e alterar

--- index.php
+++ index.php
@@ -20,8 +20,19 @@ echo json_encode($lista);*/
 /*$search = Usuario::search("uni");
 echo json_encode($search);*/
 
-$usuario = new Usuario();
+/*$usuario = new Usuario();
 $usuario->login("junior","aparecida");
+echo $usuario;*/
+
+/*$aluno = new Usuario("alun","@lun0");
+$aluno->insert();
+
+echo $aluno;*/
+
+$usuario = new Usuario();
+$usuario->loadById(6);
+$usuario->update("developer", "!@#$%&");
+
 echo $usuario;
 
 ?>
